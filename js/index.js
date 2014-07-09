29e6a03b187cefb73ebd4735c6946bb7f4e7b616
Tomi <258220040@qq.com>
2014-07-09 23:19:43 +0800

    sd

--- js/index.js
+++ js/index.js
@@ -6,27 +6,34 @@
  * To change this template use File | Settings | File Templates.
  */
 define(['./global/global'], function (g) {
+    var part16Timeout;
+
 	var IndexController = FishMVC.View.extend({
 		init: function () {
             var self = this;
+            this.doJiantou();
 		},
 		elements: {
             '.J_nextPage':'nextPage',
             '.J_part02_position':'part02Position',
             '#part02Zhizheng':'part02Zhizheng',
-            '#part03Prev':'part03Prev',
             '#part03Next':'part03Next',
             '#part03Car':'part03Car',
             '.part03-cart .img':'cartImg',
             '.part03-desc .img':'cartImgDesc',
             '.touchslider-item img':'touchsliderItemImg',
-            '#part03Prev img':'part03Prev',
-            '#part03Next img':'part03Next'
+            '.part03Prev img':'part03Prev',
+            '.part03Next img':'part03Next',
+            '.partJiantou img':'partJiantou',
+            '.part16-phone .part02-button':'part16PhoneButton',
+            '.part16-car img':'part16Car',
+            '.part16-people img':'part16People'
 		},
 		events: {
             'click nextPage':'doNextPage',
             'click part02Position':'doPart02Position',
-            'click part03Next':'doPart03Next'
+            'click part03Next':'doPart03Next',
+            'touchstart part16PhoneButton':'doPart16PhoneButton'
 		},
 
         doNextPage:function(target){
@@ -67,11 +74,15 @@ define(['./global/global'], function (g) {
         },
 
         initPart03:function(){
-            var self = this;
             this.touchsliderItemImg.show();
 
             $(".touchslider").touchSlider({/*options*/});
 
+            this.doPart03Jiantou();
+        },
+
+        doPart03Jiantou:function(){
+            var self = this;
             var i = 0;
             setInterval((function(){
                 self['part03Prev'].css('opacity',0.5);
@@ -83,11 +94,70 @@ define(['./global/global'], function (g) {
                 if(i>2){
                     i=0;
                 }
-
                 console.log(i);
 
             }),300)
+        },
+
+        doJiantou:function(){
+            var self = this;
+            var i = 0;
+            setInterval((function(){
+                self['partJiantou'].css('opacity',0.5);
+                self['partJiantou'].eq(i).css('opacity',1);
+                i++;
+
+                if(i>2){
+                    i=0;
+                }
+
+            }),300)
+        },
+
+        doPart16PhoneButton:function(target){
+            var temp = target.data('temp'),self = this;
+            temp = temp.split(',');
+
+            if(self.part16Car.hasClass(temp[0])){
+                return false;
+            }
+
+            clearTimeout(part16Timeout);
+
+            self.part16People.fadeIn('fast');
+
+            self.part16Car.css('-webkit-transition','-webkit-transform 0s ease-out');
+
+
+            self.part16Car.removeClass();
+
+            part16Timeout = setTimeout(function(){
+                self.part16Car.css('-webkit-transition','-webkit-transform '+temp[1]+'s ease-out');
+            },200);
+
+            part16Timeout = setTimeout(function(){
+                self.part16Car[0].className = temp[0];
+
+                part16Timeout = setTimeout(function(){
+                    self.part16People.eq(temp[2]).fadeOut('slow');
+
+                    self.part16Car.css('-webkit-transition','-webkit-transform '+3/Number(temp[1])+'s ease-in');
+
+                    part16Timeout = setTimeout(function(){
+                        self.part16Car[0].className = 'part16-car-end';
+
+                        part16Timeout = setTimeout(function(){
+                            self.part16People.eq(temp[2]).fadeIn('fast');
+                            self.part16Car.removeClass().removeAttr('style');
+                        },3/Number(temp[1])*1000);
+
+                    },500);
+
+                },Number(temp[1])*1000);
+
+            },400);
         }
+
 	});
 	var indexController = new IndexController({el: $('.wrapper')});
 });
