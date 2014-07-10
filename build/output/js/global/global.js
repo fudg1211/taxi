70fa9693c6a631d25256aa0c080c39a2c08f7686
Tomi <258220040@qq.com>
2014-07-10 23:52:34 +0800

    是

--- build/output/js/global/global.js
+++ build/output/js/global/global.js
@@ -1,15 +1,1 @@
-/**
- * Created with IntelliJ IDEA.
- * User: fudongguang
- * Date: 13-1-17
- * Time: 下午3:46
- * 通用函数.
- */
-
-
-
-define(['./common','./configs', './storage', './hack'], function (common,configs, storage, hack) {
-	var g = {};
-	$.extend(g,common,configs,storage,hack);
-	return g;
-});
+define(["./common","./configs","./storage","./hack"],function(b,c,d,e){var a={};$.extend(a,b,c,d,e);return a});
